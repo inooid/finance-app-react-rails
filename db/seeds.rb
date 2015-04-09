31481dfcb474074fdb0c67af76d400f2b8e3d47e
Boyd Dames <JustDrawCats@gmail.com>
2015-04-09 20:33:37 +0200

    Add some date to work with (seeds)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Receipt.create([
+  {
+    euros: 10,
+    cents: 10,
+    date: DateTime.new(2015, 2, 4, 19, 27)
+  }, {
+    euros: 12,
+    cents: 79,
+    date: DateTime.new(2015, 2, 9, 18, 57)
+  }, {
+    euros: 1,
+    cents: 24,
+    date: DateTime.new(2015, 2, 16, 18, 5)
+  }, {
+    euros: 6,
+    cents: 62,
+    date: DateTime.new(2015, 2, 18, 18, 36)
+  }, {
+    euros: 13,
+    cents: 26,
+    date: DateTime.new(2015, 2, 27, 18, 54)
+  }, {
+    euros: 1,
+    cents: 59,
+    date: DateTime.new(2015, 3, 4, 18, 9)
+  }
+])
