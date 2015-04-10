761885168a8a1c009b841e2ed995032953cae414
Boyd Dames <JustDrawCats@gmail.com>
2015-04-10 11:17:39 +0200

    Add month selection for Receipt API
    […]/api/v1/receipts/by_month
    […]/api/v1/receipts/by_month?date=2015-02-15
    Format is everything that Date.parse can parse, else it will go to
    Date.today

--- app/controllers/api/v1/receipts_controller.rb
+++ app/controllers/api/v1/receipts_controller.rb
@@ -6,7 +6,15 @@ module Api
       end
 
       def by_month
-        render json: Receipt.by_month(params[:date])
+        render json: Receipt.by_month(date_param)
+      end
+
+      private
+
+      def date_param
+        Date.parse(params.permit(:date)[:date] || Date.today.to_s)
+      rescue ArgumentError
+        Date.today
       end
     end
   end
