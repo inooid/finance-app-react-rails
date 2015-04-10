19927e3b9a018b013b77f6dd830cc123a4c58275
Boyd Dames <JustDrawCats@gmail.com>
2015-04-10 15:01:54 +0200

    Add default value for date if not set

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,8 +1,14 @@
 class Receipt < ActiveRecord::Base
+  before_save :default_values
+
   validates :euros, presence: true
   validates :cents, presence: true
 
   def self.by_month(date = Date.today)
     where('date >= ? AND date <= ?', date.beginning_of_month, date.end_of_month)
+  end
+
+  def default_values
+    self.date ||= DateTime.now
   end
 end
