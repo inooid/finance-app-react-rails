f551f0acb261de2d6c98615ad68fceaf0a7f1a4a
Boyd Dames <JustDrawCats@gmail.com>
2015-04-10 15:01:42 +0200

    Add model validations

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,4 +1,7 @@
 class Receipt < ActiveRecord::Base
+  validates :euros, presence: true
+  validates :cents, presence: true
+
   def self.by_month(date = Date.today)
     where('date >= ? AND date <= ?', date.beginning_of_month, date.end_of_month)
   end
