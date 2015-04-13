86ae8655059958973d946793c923729c47f2c4d2
Boyd Dames <JustDrawCats@gmail.com>
2015-04-13 17:20:16 +0200

    Added more data to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# DateTime.new(YYYY, MM, DD, HH, MM)
 Receipt.create([
   {
     euros: 10,
@@ -31,5 +32,61 @@ Receipt.create([
     euros: 1,
     cents: 59,
     date: DateTime.new(2015, 3, 4, 18, 9)
+  }, {
+    euros: 4,
+    cents: 77,
+    date: DateTime.new(2015, 3, 16, 15, 12)
+  }, {
+    euros: 6,
+    cents: 37,
+    date: DateTime.new(2015, 3, 17, 14, 8)
+  }, {
+    euros: 7,
+    cents: 92,
+    date: DateTime.new(2015, 2, 7, 15, 3)
+  }, {
+    euros: 5,
+    cents: 18,
+    date: DateTime.new(2015, 3, 24, 19, 35)
+  }, {
+    euros: 1,
+    cents: 73,
+    date: DateTime.new(2015, 3, 17, 14, 58)
+  }, {
+    euros: 19,
+    cents: 39,
+    date: DateTime.new(2015, 4, 1, 17, 36)
+  }, {
+    euros: 8,
+    cents: 89,
+    date: DateTime.new(2015, 4, 3, 11, 50)
+  }, {
+    euros: 10,
+    cents: 6,
+    date: DateTime.new(2015, 3, 10, 19, 3)
+  }, {
+    euros: 15,
+    cents: 43,
+    date: DateTime.new(2015, 2, 24, 19, 2)
+  }, {
+    euros: 9,
+    cents: 69,
+    date: DateTime.new(2015, 2, 2, 18, 49)
+  }, {
+    euros: 11,
+    cents: 14,
+    date: DateTime.new(2015, 3, 3, 19, 10)
+  }, {
+    euros: 1,
+    cents: 29,
+    date: DateTime.new(2015, 4, 8, 16, 40)
+  }, {
+    euros: 4,
+    cents: 91,
+    date: DateTime.new(2015, 3, 13, 18, 40)
+  }, {
+    euros: 1,
+    cents: 35,
+    date: DateTime.new(2015, 4, 10, 18, 07)
   }
 ])
