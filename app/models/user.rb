13cfa6a9b62349fb3a9c605955f15fe52d7865e2
Boyd Dames <JustDrawCats@gmail.com>
2015-04-23 10:45:07 +0200

    Add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+# The representation of the user
 class User < ActiveRecord::Base
+  attr_accessor :password
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
+  # - Validations -------------------------------------------------------------#
+  validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
+  validates :email, presence: true, uniqueness: true, format: EMAIL_REGEX
+  validates :password, confirmation: true # password_confirmation attr
+  validates_length_of :password, in: 6..100, on: :create
 end
