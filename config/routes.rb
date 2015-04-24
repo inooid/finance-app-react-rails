b1e98881ddeadedc176810d0b502dbb95f60bb77
Boyd Dames <JustDrawCats@gmail.com>
2015-04-24 17:16:19 +0200

    Remove unused by_month route

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :receipts, except: :show do
-        collection do
-          get 'by_month'
-        end
-      end
+      resources :receipts, except: :show
     end
   end
 end
