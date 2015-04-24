547473f3e9a91110f81e1e4b20d7ac86cf446479
Boyd Dames <JustDrawCats@gmail.com>
2015-04-24 17:21:41 +0200

    Add authentication route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      # Authentication
+      post '/authenticate', to: 'authentication#authenticate'
+
+      # Receipts
       resources :receipts, except: :show
     end
   end
