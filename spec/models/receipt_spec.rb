fe9b44f359803388766ad841834eea0ae166001a
Inooid <justdrawcats@gmail.com>
2015-04-26 21:23:23 +0200

    Add first Receipt specs

--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -1,40 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Receipt, type: :model do
-  context '.by_month' do
-    before do
-      Timecop.freeze
-    end
+  let(:receipt) { FactoryGirl.build(:receipt) }
+  subject { receipt }
 
-    context 'when it has receipts' do
-      # before do
-      #   create(:receipt, date: DateTime.now.beginning_of_month)
-      #   create(:receipt, date: DateTime.now.beginning_of_month + 2.days)
-      #   create(:receipt, date: DateTime.now.beginning_of_month + 5.weeks)
-      #   create(:receipt, date: DateTime.now.beginning_of_month + 2.months)
-      #   create(:receipt, date: DateTime.now.beginning_of_month - 1.months)
-      # end
-
-      # context 'when no date param' do
-      #   subject { Receipt.by_month }
-
-      #   let(:expected) do
-      #     [build(:receipt, date: DateTime.now.beginning_of_month),
-      #      build(:receipt, date: DateTime.now.beginning_of_month + 2.days)]
-      #   end
-
-      #   it 'returns an array with receipts' do
-      #     expect(subject).to match_array(expected)
-      #   end
-      # end
-
-      # context 'when has date param that has DateTime two months from now' do
-      #   subject { Receipt.by_month(DateTime.now + 2.months) }
-
-      #   it 'should find one receipt' do
-      #     expect(subject.count).to eq(1)
-      #   end
-      # end
-    end
+  describe 'amount' do
+    it { is_expected.to validate_presence_of(:amount) }
   end
 end
