1161f94b2656a51856d2ebccba9cc551216a88cb
Inooid <justdrawcats@gmail.com>
2015-04-26 20:55:19 +0200

    Add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.build(:user) }
+  subject { user }
+
+  describe 'username' do
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_uniqueness_of(:username) }
+    it { is_expected.to validate_length_of(:username).is_at_least(3) }
+    it { is_expected.to validate_length_of(:username).is_at_most(36) }
+  end
+
+  describe 'password' do
+    it { is_expected.to have_secure_password }
+  end
+
+  describe 'email' do
+    it { is_expected.to validate_presence_of(:email) }
+
+    context 'when it has an valid e-mail' do
+      it { is_expected.to be_valid }
+    end
+
+    context 'when it has an invalid e-mail' do
+      before { user.email = 'abc@asd' }
+      subject { user }
+
+      it { is_expected.to be_invalid }
+    end
+  end
 end
