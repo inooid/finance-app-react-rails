c8eb2e54737ff48a6f445cb6454747d46dd8d78d
Inooid <justdrawcats@gmail.com>
2015-04-27 21:04:57 +0200

    Add amount, date and #month rspec tests

--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -1,10 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Receipt, type: :model do
-  let(:receipt) { FactoryGirl.build(:receipt) }
-  subject { receipt }
+  # - Variables ---------------------------------------------------------------#
+  let(:receipt) { FactoryGirl.create(:receipt) }
+  let(:now) { Timecop.freeze(Time.zone.now) }
 
+  # - Fields ------------------------------------------------------------------#
+  # -- 1. amount --------------------------------------------------------------#
   describe 'amount' do
+    subject { receipt }
     it { is_expected.to validate_presence_of(:amount) }
+  end
+
+  # -- 2. date ----------------------------------------------------------------#
+  describe 'date' do
+    subject { receipt.date.is_a?(ActiveSupport::TimeWithZone) }
+
+    it 'is expected to be a \'ActiveSupport::TimeWithZone\'' do
+      is_expected.to eq(true)
+    end
+  end
+
+  # - Methods -----------------------------------------------------------------#
+  # -- 1. #month --------------------------------------------------------------#
+  describe '#month' do
+    context 'when having 2 receipts in this month and 1, two months ago' do
+      before do
+        2.times { FactoryGirl.create(:receipt) }
+        FactoryGirl.create(:receipt, date: (now - 2.months))
+      end
+
+      it 'should have 3 total receipts' do
+        expect(Receipt.all.count).to eq(3)
+      end
+
+      context '#month with the current month number as param' do
+        subject { Receipt.month(now.month) }
+
+        it 'should find 2 receipts' do
+          expect(subject.count).to eq(2)
+        end
+      end
+
+      context '#month with month number from 2 months ago as param' do
+        subject { Receipt.month((now - 2.months).month) }
+
+        it 'should find 1 receipt for the month' do
+          expect(subject.count).to eq(1)
+        end
+      end
+    end
   end
 end
