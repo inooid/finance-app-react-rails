eedd816b5c674c7d292a3c075dbf04e621ca056a
Inooid <justdrawcats@gmail.com>
2015-05-03 21:52:16 +0200

    Fix old seedings

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,84 +9,64 @@
 # DateTime.new(YYYY, MM, DD, HH, MM)
 Receipt.create([
   {
-    euros: 10,
-    cents: 10,
+    amount: 10.10,
     date: DateTime.new(2015, 2, 4, 19, 27)
   }, {
-    euros: 12,
-    cents: 79,
+    amount: 12.79,
     date: DateTime.new(2015, 2, 9, 18, 57)
   }, {
-    euros: 1,
-    cents: 24,
+    amount: 1.24,
     date: DateTime.new(2015, 2, 16, 18, 5)
   }, {
-    euros: 6,
-    cents: 62,
+    amount: 6.62,
     date: DateTime.new(2015, 2, 18, 18, 36)
   }, {
-    euros: 13,
-    cents: 26,
+    amount: 13.26,
     date: DateTime.new(2015, 2, 27, 18, 54)
   }, {
-    euros: 1,
-    cents: 59,
+    amount: 1.59,
     date: DateTime.new(2015, 3, 4, 18, 9)
   }, {
-    euros: 4,
-    cents: 77,
+    amount: 4.77,
     date: DateTime.new(2015, 3, 16, 15, 12)
   }, {
-    euros: 6,
-    cents: 37,
+    amount: 6.37,
     date: DateTime.new(2015, 3, 17, 14, 8)
   }, {
-    euros: 7,
-    cents: 92,
+    amount: 7.92,
     date: DateTime.new(2015, 2, 7, 15, 3)
   }, {
-    euros: 5,
-    cents: 18,
+    amount: 5.18,
     date: DateTime.new(2015, 3, 24, 19, 35)
   }, {
-    euros: 1,
-    cents: 73,
+    amount: 1.73,
     date: DateTime.new(2015, 3, 17, 14, 58)
   }, {
-    euros: 19,
-    cents: 39,
+    amount: 19.39,
     date: DateTime.new(2015, 4, 1, 17, 36)
   }, {
-    euros: 8,
-    cents: 89,
+    amount: 8.89,
     date: DateTime.new(2015, 4, 3, 11, 50)
   }, {
-    euros: 10,
-    cents: 6,
+    amount: 10.6,
     date: DateTime.new(2015, 3, 10, 19, 3)
   }, {
-    euros: 15,
-    cents: 43,
+    amount: 15.43,
     date: DateTime.new(2015, 2, 24, 19, 2)
   }, {
-    euros: 9,
-    cents: 69,
+    amount: 9.69,
     date: DateTime.new(2015, 2, 2, 18, 49)
   }, {
-    euros: 11,
-    cents: 14,
+    amount: 11.14,
     date: DateTime.new(2015, 3, 3, 19, 10)
   }, {
-    euros: 1,
-    cents: 29,
+    amount: 1.29,
     date: DateTime.new(2015, 4, 8, 16, 40)
   }, {
-    euros: 4,
-    cents: 91,
+    amount: 4.91,
     date: DateTime.new(2015, 3, 13, 18, 40)
   }, {
-    euros: 1,
-    cents: 35,
+    amount: 1.35,
     date: DateTime.new(2015, 4, 10, 18, 07)
   }
 ])
